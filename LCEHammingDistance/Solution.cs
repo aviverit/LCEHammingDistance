2b6d194f86053ced6b54d9a8ca1610f92e820d73
aviverit <andrew.viverito@gmail.com>
2017-07-04 23:39:48 -0500

    v.0.1.1 "ints converted to binary string representation"

---
+++ LCEHammingDistance/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCEHammingDistance
+{
+    class Solution
+    {
+        public int HammingDistance(int x, int y)
+        {
+            int counter = 0;
+
+            String binaryX = Convert.ToString(x, 2);
+            String binaryY = Convert.ToString(y, 2);
+
+            return counter;
+        }
+    }
+}
